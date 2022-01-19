5e0a6c7142eb5e915abc927b9f95fea362076962
Shahana Akter <98011934+Shahana-Sumi@users.noreply.github.com>
2022-01-19 13:40:02 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		
+		<title>Result_Sheet</title>
+	</head>
+	<body>
+		<?php 
+			$marks = 98;
+		
+			if($marks >=80 && $marks <=100) {
+				echo "Obtained A+";
+			}
+
+
+		 ?>
+
+	</body>
+</html>	
